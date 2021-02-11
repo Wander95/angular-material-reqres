30140a4d418f7972ccf7a2c10ad064bcb442c928
Wander95 <wanders1995@hotmail.com>
2021-02-11 07:54:29 -0400

    💄 Remove default styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { ClientComponent } from './components/client/client.component';
+import { FormControl, FormsModule } from '@angular/forms';
+import { ClientService } from './service/client.service';
+import { HttpClientModule } from '@angular/common/http';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MaterialModules } from './material.module';
+import { ClientTableComponent } from './components/client-table/client-table.component';
+import { ClientEditFormComponent } from './components/client-edit-form/client-edit-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    ClientTableComponent,
+    ClientEditFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModules,
+    FormsModule,
+    MatSliderModule,
+    HttpClientModule,
+    ServiceWorkerModule.register(
+      'ngsw-worker.js',
+      { enabled: environment.production }
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ClientService ],
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
